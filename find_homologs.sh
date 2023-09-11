9f9ae2686e4e6434990e64b245e73c05b8fb3131
jejiki <jennajwjwk@gmail.com>
2023-09-10 23:11:03 -0400

    added data to the find_homologs repo

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 #usage: ./find_perfect_matches.sh <query file> <subject file> <output file> 
-blastn -query $1 -subject $2 -task blastn-short -outfmt '6 std sseq' -out $3
+blastx -query $1 -subject $2 -task blastx-fast -outfmt '6 std sseq' -out $3
 
-awk '$3 == 100.000 && $4 == $8' $3 | wc -l 
+#awk '$3 >= 30.000 && $4 == $8' $3 | wc -l 
