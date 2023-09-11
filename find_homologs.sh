6d7a3202c757225c5389c89cb1cce57dcf41171c
jejiki <jennajwjwk@gmail.com>
2023-09-11 15:42:57 -0400

    removed int() from the awk command

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 #usage: ./find_homologs <query file> <subject file> <output file> 
-tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' -out $3
-awk '$3 > 30 && int($4/$13) > 0.9' $3 | wc -l 
+temp=temp.txt
+echo ' ' > $temp
+touch $temp
+tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen' -out $3
+awk '$3 > 30 && $4/$13 > 0.9' $3 | wc -l
