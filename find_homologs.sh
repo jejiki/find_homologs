d463ee3b5b9183413cf8220e9b905638a1ca3515
jejiki <jennajwjwk@gmail.com>
2023-09-11 14:24:44 -0400

    added the command that worked in the terminal to the script

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/zsh
 #usage: ./find_perfect_matches.sh <query file> <subject file> <output file> 
-tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen'
-awk '($3*1) > 30.000 && ($4*1)/($13*1) * 100 > 90.000' > $3 | wc -l 
+tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen' -out $3
+awk '$3 > 30 && int($4/$13) > 0.9' | wc -l 
+
+
