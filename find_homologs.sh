3a826d59a9bf7d9368fdc4f04132698797dc2c24
jejiki <jennajwjwk@gmail.com>
2023-09-11 11:14:04 -0400

    deleted possibly faulty data

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/zsh
 #usage: ./find_perfect_matches.sh <query file> <subject file> <output file> 
-tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' -out $3
-
-awk '$3 > 30.000' > $3 | wc -l 
-# && ($4/$13) > 90.000
+tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen' -out $3
+awk '$3 > 30.000 && ($4/$13 * 100) > 90.000' > $3 | wc -l 
