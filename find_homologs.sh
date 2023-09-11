43beec23483fbbcb241c0d29b9246b53214dc09d
jejiki <jennajwjwk@gmail.com>
2023-09-11 11:23:06 -0400

    added new dataset

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 #usage: ./find_perfect_matches.sh <query file> <subject file> <output file> 
-tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen' -out $3
-awk '$3 > 30.000 && ($4/$13 * 100) > 90.000' > $3 | wc -l 
+tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen'
+awk '($3*1) > 30.000 && ($4*1)/($13*1) * 100 > 90.000' > $3 | wc -l 
