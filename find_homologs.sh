6468f10f067a79b180c9a5af3cda40712acad91f
jejiki <jennajwjwk@gmail.com>
2023-09-11 14:32:04 -0400

    successfully debugged. Outputs the number of matched homologs.

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/zsh
-#usage: ./find_perfect_matches.sh <query file> <subject file> <output file> 
-tblastn -query $1 -subject $2 -task tblastn-fast -outfmt '6 std qlen' -out $3
-awk '$3 > 30 && int($4/$13) > 0.9' | wc -l 
-
-
+#usage: ./find_homologs <query file> <subject file> <output file> 
+tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' -out $3
+awk '$3 > 30 && int($4/$13) > 0.9' $3 | wc -l 
